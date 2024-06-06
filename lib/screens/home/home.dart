5065098a8d0d3a780760df4eba9798ce6de3e900
KhaledxOmar <mr.joker2017@gmail.com>
2024-06-07 01:27:03 +0300

    making a like card widget that have a list of characters & styled text and button

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,3 +1,5 @@
+import 'package:characters_app/shared/styled_button.dart';
+import 'package:characters_app/shared/styled_text.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,6 +10,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List chatacters = ['khaled', 'tameem', 'aljowharah', 'yasser', 'Ahlam', 'LULU'];
   
 
   @override
@@ -15,7 +19,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
 
       appBar: AppBar(
-        title: const Text("Your characters"),
+        title: const StyledTitle("Your characters"),
         centerTitle: true,
       ),
 
@@ -23,11 +27,24 @@ class _HomeState extends State<Home> {
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
-            const Text("characters list"),
+            //expanded to make it constrained to the screen. whatever space that is left it. 
+            Expanded( 
+              child: ListView.builder(
+                itemCount: chatacters.length,
+                itemBuilder: (_,index){
+                  return Container(
+                    color: Colors.grey[800],
+                    padding: const EdgeInsets.all(40),
+                    margin: const EdgeInsets.only(bottom: 40),
+                    child: Text(chatacters[index]),
+                  );
+                },
+              ),
+            ),
 
-            FilledButton(
+            StyledButton(
                 onPressed: (){}, 
-                child: const Text("create new"),
+                child: const StyledHeading("create new"),
                 ),
 
             
